3bb98807d21ff1ffab06f7ea7fcf2acacab7a28e
dawcr <dawcraz@gmail.com>
2025-05-18 16:00:27 +0200

    Update to latest specifications

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import sys
 
 from stats import count_characters, count_words
 
 def main():
-    path_to_file = "books/frankenstein.txt" # relative path to the book
-    book_name = path_to_file.split('/')[-1] # get the name of the book from path
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_file = sys.argv[1] # relative path to the book
     
     book_contents = get_book_contents(path_to_file) 
     num_words = count_words(book_contents)  # how many words are present in the book
     char_dict = count_characters(book_contents)    # how many times each character appears in the book, not case sensitive(all lowercase)
-    print_book_report(book_name, num_words, char_dict) # print the book report to terminal
+    print_book_report(path_to_file, num_words, char_dict) # print the book report to terminal
 
 def get_book_contents(path: str) -> str:
     """
@@ -20,7 +23,7 @@ def get_book_contents(path: str) -> str:
     with open(path) as f:
         return f.read()
 
-def print_book_report(book_name: str, words_count: int, character_dict: dict) -> None:
+def print_book_report(path_to_file: str, words_count: int, character_dict: dict) -> None:
     """
     print the report on how many times each alphabetical character appears in the book to terminal, ordered descendingly by number of appearances 
     
@@ -28,13 +31,16 @@ def print_book_report(book_name: str, words_count: int, character_dict: dict) ->
     :param words_count: the number of words in the book
     :param character_dict: a dictionary containing all alphabetical characters that appear in the book and the amount of times they appear
     """
-    print(f"--- Begin report of {book_name} ---")
-    print(f"{words_count} words found in the document" if words_count > 1 
-          else "1 word found in the document" if words_count == 1 
-          else "no words found in the document")
-    print('\n') # print 2 newlines
+    book_name = path_to_file.split('/')[-1] # get the name of the book from path
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words_count} total words" if words_count > 1 
+          else "Found 1 word in total" if words_count == 1 
+          else "No words found in the document")
+    print("--------- Character Count -------")
     for char, amount in sorted(character_dict.items(), key = lambda x: x[1], reverse = True):
-        print(f"The character '{char}' was found {amount} times" if amount > 1
-              else f"The character '{char}' was found {amount} time")
+        print(f"{char}: {amount}")
+    print("============= END ===============")
         
 main()
